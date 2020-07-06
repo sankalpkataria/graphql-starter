a008cc6194063a5ec344b7189036cff2b0e34840
sankalp kataria <sankalpkataria@gmail.com>
2020-07-07 01:05:05 +0530

    fixed error formatting and logging

--- apollo.js
+++ apollo.js
@@ -6,29 +6,38 @@ const { logger } = require("./errors");
 
 const { ENV, ENVIRONMENTS, ERROR, LOG_LEVELS } = constants;
 
+const formatError = (error) => {
+    const errorInfo = {
+        code: ERROR.INTERNAL_SERVER_ERROR.CODE,
+        type: ERROR.INTERNAL_SERVER_ERROR.TYPE,
+        time: new Date(),
+        path: error.path,
+        isOperational: false
+    };
+    const exception = error.extensions && error.extensions.exception;
+    if (exception) {
+        errorInfo.code = exception.code;
+        errorInfo.type = exception.type;
+        errorInfo.time = exception.time;
+        errorInfo.isOperational = exception.isOperational;
+        if (ENV === ENVIRONMENTS.PRODUCTION) {
+            errorInfo.stacktrace = JSON.stringify(exception.stacktrace, null, 2);
+            delete exception.stacktrace;
+        }
+        logger.log(LOG_LEVELS.ERROR, error.message, errorInfo);
+        return exception;
+    }
+    
+    // For errors that do not reach our resolvers such as graphQL validations, etc
+    logger.log(LOG_LEVELS.ERROR, error.message, errorInfo);
+    return error;
+};
+
 const apolloServer = new ApolloServer({
     typeDefs,
     resolvers,
     context: async ({ req }) => ({ authToken: req.headers.authorization }),
-    formatError: (error) => {
-        if (error.extensions && error.extensions.exception && error.extensions.exception.type) {
-            if (ENV === ENVIRONMENTS.PRODUCTION) {
-                delete error.extensions.exception.stacktrace;
-            }
-            return error.extensions.exception;
-        }
-        logger.log(
-            LOG_LEVELS.ERROR,
-            error.message,
-            {
-                code: ERROR.INTERNAL_SERVER_ERROR.CODE,
-                type: ERROR.INTERNAL_SERVER_ERROR.TYPE,
-                time: new Date(),
-                isOperational: false
-            }
-        );
-        return error;
-    },
+    formatError: formatError,
     schemaDirectives: {
         // define directives here
     },
